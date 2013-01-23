bab098170e5f398cc1c53313aa6409b1d4226a64
TJ Phillips <tncball33@gmail.com>
2013-01-23 15:11:07 -0600

    commit from branch.

---
+++ GitUseExample/MapForItinerary.h
@@ -0,0 +1,15 @@
+//
+//  MapForItinerary.h
+//  GitUseExample
+//
+//  Created by Trevor Phillips on 1/23/13.
+//  Copyright (c) 2013 Austin Peay State University GIS Center. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+
+
+@end
